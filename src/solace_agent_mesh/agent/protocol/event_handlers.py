16e2c18312c645e8cc60f585090e62582378060b
Greg Meldrum <gregmeldrum@gmail.com>
2025-09-18 10:58:33 -0400

    Await a2a_auth_message_handler

--- src/solace_agent_mesh/agent/protocol/event_handlers.py
+++ src/solace_agent_mesh/agent/protocol/event_handlers.py
@@ -420,7 +420,7 @@ async def handle_a2a_request(component, message: SolaceMessage):
 
             try:
                 from solace_agent_mesh_enterprise.auth.input_required import a2a_auth_message_handler
-                message_handled = a2a_auth_message_handler(component, a2a_message, logical_task_id)
+                message_handled = await a2a_auth_message_handler(component, a2a_message, logical_task_id)
                 if message_handled:
                     message.call_acknowledgements()
                     log.info(
