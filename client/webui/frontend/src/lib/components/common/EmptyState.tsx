80f0fdbfaa7919875cca1d441a11d24d434af9a3
Raman Gupta <raman.gupta@solace.com>
2025-10-08 09:46:26 -0400

    feat: add basic empty state component

---
+++ client/webui/frontend/src/lib/components/common/EmptyState.tsx
@@ -0,0 +1,37 @@
+import type { VariantProps } from "class-variance-authority";
+import { Button } from "@/lib/components/ui/button";
+import type { buttonVariants } from "@/lib/components/ui/button";
+
+type ButtonVariant = VariantProps<typeof buttonVariants>["variant"];
+
+export interface ButtonWithCallback {
+    text: string;
+    variant: ButtonVariant;
+    onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+interface EmptyStateProps {
+    title: string;
+    subtitle?: string;
+    buttons?: ButtonWithCallback[];
+}
+
+function EmptyState({ title, subtitle, buttons }: EmptyStateProps) {
+    return (
+        <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+            <p className="text-2xl">{title}</p>
+            {subtitle ? <p className="text-base">{subtitle}</p> : null}
+
+            <div className="flex gap-2">
+                {buttons &&
+                    buttons.map(({ text, variant, onClick }) => (
+                        <Button variant={variant} onClick={onClick}>
+                            {text}
+                        </Button>
+                    ))}
+            </div>
+        </div>
+    );
+}
+
+export { EmptyState };
