17a46454dff85cb62ef80e9a57eaf998c18cbd13
Raman Gupta <raman.gupta@solace.com>
2025-10-10 10:17:50 -0400

    feat: add theme provider as decorator

--- client/webui/frontend/.storybook/preview.tsx
+++ client/webui/frontend/.storybook/preview.tsx
@@ -1,14 +1,27 @@
-import type { Preview } from "@storybook/react-vite";
+import React from "react";
 import "../src/lib/index.css";
+import "../src/App.css";
+
+import type { Preview } from "@storybook/react-vite";
+import { ThemeProvider } from "../src/lib/providers/ThemeProvider";
 
 const preview: Preview = {
+    decorators: [
+        Story => (
+            <ThemeProvider>
+                <Story />
+            </ThemeProvider>
+        ),
+    ],
     parameters: {
         actions: { argTypesRegex: "^on[A-Z].*" },
+
         controls: {
             matchers: {
                 color: /(background|color)$/i,
                 date: /Date$/i,
             },
+            expanded: true,
         },
         backgrounds: {
             default: "light",
