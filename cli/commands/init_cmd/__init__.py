20edf77613bdf20071039ce942657212c67ea49b
Robert Zuchniak <robert.zuchniak@solace.com>
2025-10-14 09:07:28 -0400

    chore: remove comments

--- cli/commands/init_cmd/__init__.py
+++ cli/commands/init_cmd/__init__.py
@@ -165,11 +165,6 @@ def run_init_flow(skip_interactive: bool, use_web_based_init_flag: bool, **cli_o
                 project_root, opts, skip, defs
             ),
         ),
-        # Removed for now, as SQL is default and auto-handled
-        #(
-        #    "Database Setup",
-        #    lambda opts, defs, skip: database_setup_step(project_root, opts, skip),
-        #),
         (
             ".env File Creation",
             lambda opts, defs, skip: create_env_file(project_root, opts, skip),
