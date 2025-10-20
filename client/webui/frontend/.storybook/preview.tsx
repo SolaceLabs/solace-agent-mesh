b19c89937185ce9324c2d44b760d840ec7816166
Raman Gupta <raman.gupta@solace.com>
2025-10-20 12:03:42 -0400

    feat: use with provider decorator in storybook config

--- client/webui/frontend/.storybook/preview.tsx
+++ client/webui/frontend/.storybook/preview.tsx
@@ -4,15 +4,10 @@ import "../src/App.css";
 
 import type { Preview } from "@storybook/react-vite";
 import { ThemeProvider } from "../src/lib/providers/ThemeProvider";
+import { withProviders } from "../src/stories/decorators/withProviders";
 
 const preview: Preview = {
-    decorators: [
-        Story => (
-            <ThemeProvider>
-                <Story />
-            </ThemeProvider>
-        ),
-    ],
+    decorators: [withProviders],
     parameters: {
         actions: { argTypesRegex: "^on[A-Z].*" },
 
