87037de2aa759bed301d140c4c8658befe85687b
RohanSreelesh <Rohansreelesh02@gmail.com>
2025-10-22 13:10:42 -0400

    remove complicated project creation step

--- client/webui/frontend/src/lib/components/projects/CreateProjectWizard/index.ts
+++
@@ -1,5 +0,0 @@
-export { CreateProjectWizard } from "./CreateProjectWizard";
-export { StepIndicator } from "./StepIndicator";
-export { ProjectDetailsStep } from "./steps/ProjectDetailsStep";
-export { FilesArtifactsStep } from "./steps/FilesArtifactsStep";
-export { ReviewCreateStep } from "./steps/ReviewCreateStep";
