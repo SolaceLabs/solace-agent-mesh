f28ea13cfc1ee12a54f4a523783ad3b2a8aa8225
Raman Gupta <raman.gupta@solace.com>
2025-10-24 17:12:54 -0400

    feat: add message banner story with long message

--- client/webui/frontend/src/stories/MessageBanner.stories.tsx
+++ client/webui/frontend/src/stories/MessageBanner.stories.tsx
@@ -87,3 +87,15 @@ export const DismissibleBannerWithAction: Story = {
         onDismiss: () => alert("Banner will be dismissed"),
     },
 };
+
+export const LongMessage: Story = {
+    args: {
+        variant: "info",
+        dismissible: true,
+        message:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        buttonText: "Click here",
+        action: () => alert("This action will do something"),
+        onDismiss: () => alert("Banner will be dismissed"),
+    },
+};
