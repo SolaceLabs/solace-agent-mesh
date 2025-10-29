1c8a4762a95a056336363a663e05df9a56684523
Raman Gupta <raman.gupta@solace.com>
2025-10-29 15:25:02 -0400

    chore: add error dialog component from enterprise

---
+++ client/webui/frontend/src/lib/components/common/ErrorDialog.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/lib/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/lib/components/ui/dialog";
+import { CircleX } from "lucide-react";
+
+interface ErrorDialogProps {
+    title: string;
+    error: string;
+    errorDetails?: string;
+    onClose: () => void;
+}
+
+export const ErrorDialog: React.FC<ErrorDialogProps> = ({ title, error, errorDetails, onClose }) => {
+    return (
+        <Dialog open={true} onOpenChange={() => onClose()}>
+            <DialogContent className="w-xl max-w-xl sm:max-w-xl">
+                <DialogHeader>
+                    <DialogTitle className="flex max-w-[400px] flex-row gap-1">{title}</DialogTitle>
+                </DialogHeader>
+                <div className="flex flex-row items-center gap-2 py-4">
+                    <CircleX className="h-6 w-6 flex-shrink-0 self-start text-[var(--color-error-wMain)]" />
+                    <div>{error}</div>
+                </div>
+                {errorDetails && <div className="px-4 py-2 text-[var(--color-secondary-text-wMain)]">{errorDetails}</div>}
+                <div className="flex justify-end">
+                    <Button variant="outline" testid="closeButton" type="button" title="Close" onClick={() => onClose()}>
+                        Close
+                    </Button>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+};
