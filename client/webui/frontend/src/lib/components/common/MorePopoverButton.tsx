f18363c01ae34803ae33975e45371c906bf59003
Raman Gupta <raman.gupta@solace.com>
2025-11-04 09:32:39 -0500

    cleanup: remove unnecessary components

--- client/webui/frontend/src/lib/components/common/MorePopoverButton.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/lib/components/ui/button";
-import { Ellipsis } from "lucide-react";
-
-export const MorePopoverButton = (
-    <Button variant="ghost" tooltip="More">
-        <Ellipsis className="h-5 w-5" />
-    </Button>
-);
--- client/webui/frontend/src/lib/components/common/index.ts
+++ client/webui/frontend/src/lib/components/common/index.ts
@@ -4,6 +4,4 @@ export { ErrorDialog } from "./ErrorDialog";
 export { LoadingBlocker } from "./LoadingBlocker";
 export { MarkdownHTMLConverter } from "./MarkdownHTMLConverter";
 export { MessageBanner } from "./MessageBanner";
-export { MorePopoverButton } from "./MorePopoverButton";
-export { PaginationControls } from "./PaginationControls";
 export * from "./messageColourVariants";
