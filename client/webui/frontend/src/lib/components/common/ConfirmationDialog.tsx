6943386a1f4252b29e07106b450c83369e956db4
Raman Gupta <raman.gupta@solace.com>
2025-11-17 14:29:11 -0500

    fix: simplify prop type for confirmation dialog

--- client/webui/frontend/src/lib/components/common/ConfirmationDialog.tsx
+++ client/webui/frontend/src/lib/components/common/ConfirmationDialog.tsx
@@ -2,39 +2,29 @@ import { Button } from "@/lib/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/lib/components/ui/dialog";
 import { DialogClose } from "@radix-ui/react-dialog";
 
-interface BaseDialogProps {
+/* The following variable definitions are mutually exclusive and follow this precedence
+ * open, openChange > trigger > triggerText
+ *
+ * Lower precedence config values will be ignored
+ * */
+export interface ConfirmationDialogProps {
     title: string;
     message: string | React.ReactNode;
+    triggerText?: string;
+    trigger?: React.ReactNode;
+    open?: boolean;
+    onOpenChange?: (open: boolean) => void;
     onConfirm: () => void;
     onClose: () => void;
 }
 
-type ConfirmationDialogProps =
-    | (BaseDialogProps & {
-          triggerText: string;
-          trigger?: never;
-          open?: never;
-          onOpenChange?: never;
-      })
-    | (BaseDialogProps & {
-          trigger: React.ReactNode;
-          triggerText?: never;
-          open?: never;
-          onOpenChange?: never;
-      })
-    | (BaseDialogProps & {
-          trigger?: never;
-          triggerText?: never;
-          open: boolean;
-          onOpenChange: (open: boolean) => void;
-      });
-
 export const ConfirmationDialog: React.FC<ConfirmationDialogProps> = ({ title, message, triggerText, trigger, onClose, onConfirm, open, onOpenChange }) => {
     const hasTrigger = trigger || triggerText;
+    const isExternallyControlled = open && onOpenChange;
 
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-            {hasTrigger && <DialogTrigger asChild>{trigger ?? <Button>{triggerText}</Button>}</DialogTrigger>}
+            {hasTrigger && !isExternallyControlled && <DialogTrigger asChild>{trigger ?? <Button>{triggerText}</Button>}</DialogTrigger>}
             <DialogContent className="w-xl max-w-xl sm:max-w-xl">
                 <DialogHeader>
                     <DialogTitle className="flex max-w-[400px] flex-row gap-1">{title}</DialogTitle>
