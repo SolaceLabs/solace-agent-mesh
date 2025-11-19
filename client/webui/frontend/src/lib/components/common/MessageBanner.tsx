f71f2697a46e15b95d67fbf233861994816c0aeb
Raman Gupta <raman.gupta@solace.com>
2025-11-19 14:26:43 -0500

    f: simplify message banner component

--- client/webui/frontend/src/lib/components/common/MessageBanner.tsx
+++ client/webui/frontend/src/lib/components/common/MessageBanner.tsx
@@ -7,8 +7,6 @@ import { Button } from "@/lib/components";
 import { messageColourVariants } from "./messageColourVariants";
 import { cn } from "@/lib/utils";
 
-export const BANNER_BUTTON_STYLES = "h-min !bg-transparent p-0 font-normal text-current underline hover:!text-current/60 dark:hover:text-white";
-
 const messageBannerVariants = cva("flex items-center gap-3 px-4 py-3 text-sm font-medium transition-all border-l-4 border-solid ", {
     variants: { variant: messageColourVariants },
     defaultVariants: {
@@ -23,18 +21,9 @@ const iconMap = {
     success: CheckCircle,
 };
 
-/*
- * The following precedence is applied to the props:
- *
- * button > buttonText, action
- *
- * Props of lower precdence will be ignored
- * */
 export interface MessageBannerBaseProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof messageBannerVariants> {
-    message: string;
+    message: string | React.ReactNode;
     icon?: React.ReactNode;
-    action?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    buttonText?: string;
     button?: React.ReactNode;
     dismissible?: boolean;
     onDismiss?: () => void;
@@ -42,21 +31,16 @@ export interface MessageBannerBaseProps extends React.HTMLAttributes<HTMLDivElem
 
 export type MessageBannerProps = MessageBannerBaseProps;
 
-function MessageBanner({ className, variant = "error", message, icon, button, action, buttonText, dismissible = false, onDismiss, ...props }: MessageBannerProps) {
+function MessageBanner({ className, variant = "error", message, icon, button, dismissible = false, onDismiss, ...props }: MessageBannerProps) {
     const IconComponent = iconMap[variant || "error"];
 
     return (
         <div className={cn(messageBannerVariants({ variant, className }), "items-start")} role="alert" aria-live="polite" {...props}>
             {icon || <IconComponent className="size-5 shrink-0" />}
-            <span>{message}</span>
+            {typeof message === "string" ? <span>{message}</span> : message}
 
             <div className="ml-auto flex items-center gap-1">
-                {button ||
-                    (action && buttonText && (
-                        <Button variant="link" className={BANNER_BUTTON_STYLES} onClick={action}>
-                            {buttonText}
-                        </Button>
-                    ))}
+                {button}
                 {dismissible && onDismiss && (
                     <Button variant="link" className="h-min self-center p-0" onClick={onDismiss} aria-label="Dismiss">
                         <X className="size-3" />
