88ae6715157c962bc78c37301b3aa95e82080d41
Raman Gupta <raman.gupta@solace.com>
2025-11-25 13:30:42 -0500

    fix: msw init

--- client/webui/frontend/.storybook/preview.tsx
+++ client/webui/frontend/.storybook/preview.tsx
@@ -5,7 +5,10 @@ import type { Preview } from "@storybook/react-vite";
 import { withProviders } from "../src/stories/decorators/withProviders";
 import { initialize, mswLoader } from "msw-storybook-addon";
 
-initialize();
+initialize({
+    onUnhandledRequest: "bypass",
+    quiet: true,
+});
 
 const preview: Preview = {
     decorators: [withProviders],
