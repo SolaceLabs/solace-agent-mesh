9e9b59a7aea0860aa17f51194321691ba0a5a024
lgh-solace <linda.hillis@solace.com>
2025-12-02 10:46:19 -0500

    chore: tweaking styles

--- client/webui/frontend/src/lib/components/toast/Toast.tsx
+++ client/webui/frontend/src/lib/components/toast/Toast.tsx
@@ -12,7 +12,7 @@ export interface ToastProps {
 export function Toast({ message, type }: ToastProps) {
     return (
         <div className="transform transition-all duration-200 ease-in-out">
-            <Alert className="border-border dark:bg-accent max-w-80 rounded-sm bg-[var(--color-background-wMain)] shadow-md">
+            <Alert className="border-border max-w-80 rounded-sm bg-[var(--color-background-wMain)] text-[var(--color-primary-text-w10)] shadow-md">
                 <AlertTitle className="flex items-center text-sm">
                     {type === "error" && <AlertCircle className="mr-2 text-[var(--color-error-wMain)]" />}
                     {type === "success" && <CheckCircle className="mr-2 text-[var(--color-success-wMain)]" />}
