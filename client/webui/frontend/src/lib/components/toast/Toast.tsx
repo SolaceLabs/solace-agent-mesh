214274cc55216653dd35748b4f152a40c3f2a132
lgh-solace <linda.hillis@solace.com>
2025-12-02 11:20:23 -0500

    chore: tidying

--- client/webui/frontend/src/lib/components/toast/Toast.tsx
+++ client/webui/frontend/src/lib/components/toast/Toast.tsx
@@ -12,7 +12,7 @@ export interface ToastProps {
 export function Toast({ message, type }: ToastProps) {
     return (
         <div className="transform transition-all duration-200 ease-in-out">
-            <Alert className="light:border-none dark:border-border max-w-80 rounded-sm bg-[var(--color-background-wMain)] px-4 py-0 text-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.15)] dark:shadow-[0_4px_6px_-1px_rgba(255,255,255,0.1)]">
+            <Alert className="light:border-none dark:border-border max-w-80 min-w-58 rounded-sm bg-[var(--color-background-wMain)] px-4 py-0 text-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.15)] dark:shadow-[0_4px_6px_-1px_rgba(255,255,255,0.1)]">
                 <AlertTitle className="flex h-10 items-center">
                     {type === "error" && <AlertCircle className="mr-2 text-[var(--color-error-wMain)]" />}
                     {type === "warning" && <AlertTriangle className="mr-2 text-[var(--color-warning-wMain)]" />}
