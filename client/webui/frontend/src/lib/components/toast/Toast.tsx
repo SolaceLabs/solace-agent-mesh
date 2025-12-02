97821b253359f953fdeac809fafd0362c1e25175
lgh-solace <linda.hillis@solace.com>
2025-12-02 13:15:07 -0500

    chore: tidying notification type

--- client/webui/frontend/src/lib/components/toast/Toast.tsx
+++ client/webui/frontend/src/lib/components/toast/Toast.tsx
@@ -1,17 +1,11 @@
 import { AlertCircle, AlertTriangle, CheckCircle } from "lucide-react";
 
 import { Alert, AlertTitle } from "../ui/alert";
+import type { Notification } from "../../types";
 
-export interface ToastProps {
-    id: string;
-    message: string;
-    type?: "info" | "success" | "warning" | "error"; // error is deprecated, use a dialog instead
-    duration?: number;
-}
-
-export function Toast({ message, type }: ToastProps) {
+export function Toast({ id, message, type }: Notification) {
     return (
-        <div className="transform transition-all duration-200 ease-in-out">
+        <div id={id} className="transform transition-all duration-200 ease-in-out">
             <Alert className="light:border-none dark:border-border max-w-80 min-w-58 rounded-sm bg-[var(--color-background-wMain)] px-4 py-0 text-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.15)] dark:shadow-[0_4px_6px_-1px_rgba(255,255,255,0.1)]">
                 <AlertTitle className="flex h-10 items-center">
                     {type === "error" && <AlertCircle className="mr-2 text-[var(--color-error-wMain)]" />}
