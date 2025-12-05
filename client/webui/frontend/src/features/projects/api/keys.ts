dff329efc4f7059c220540d63fd44590a7811fb7
Raman Gupta <raman.gupta@solace.com>
2025-12-05 11:29:27 -0500

    f: add comment explain key architecture

--- client/webui/frontend/src/features/projects/api/keys.ts
+++ client/webui/frontend/src/features/projects/api/keys.ts
@@ -1,5 +1,22 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
+/*
+ * Reference for future devs:
+ *
+ * In the library documentation, there is a less verbose way recommended
+ * for creating keys.
+ *
+ * Example:
+ * You can do { all: null, export: (projectId: string) => [projectId] }
+ *
+ * This is significantly simpler than what's written below.
+ *
+ * However, this declaration is harder for the compiler to process
+ * and it exceeds the max depth for serialization.
+ *
+ * This doesn't work well with our need to share code between the community
+ * and enterprise version, so the more verbose version is recommended.
+ */
 export const projects = createQueryKeys("projects", {
     all: { queryKey: null },
     create: { queryKey: null },
