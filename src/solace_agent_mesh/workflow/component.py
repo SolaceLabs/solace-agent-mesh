49a0798684efce8f71a3c67cafc39e84fce000b2
Paul Jones <paul.jones@solace.com>
2026-02-02 12:11:53 -0500

    chore(DATAGO-114389): Rename workflow config field from 'agent_name' to 'name' (#945)
    * Use name instead of agent_name as part of workflow config. Updated docs and samples as well.
    
    * unit tests
    
    * fixed missed reference to name

--- src/solace_agent_mesh/workflow/component.py
+++ src/solace_agent_mesh/workflow/component.py
@@ -79,14 +79,14 @@ class WorkflowExecutorComponent(SamComponentBase):
         Initialize workflow executor component.
         """
         if "component_config" in kwargs and "app_config" in kwargs["component_config"]:
-            name = kwargs["component_config"]["app_config"].get("agent_name")
+            name = kwargs["component_config"]["app_config"].get("name")
             if name:
                 kwargs.setdefault("name", name)
 
         super().__init__(info, **kwargs)
 
         # Configuration
-        self.workflow_name = self.get_config("agent_name")
+        self.workflow_name = self.get_config("name")
         self.namespace = self.get_config("namespace")
         workflow_config = self.get_config("workflow")
 
