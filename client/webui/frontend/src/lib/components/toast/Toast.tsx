5bc390e3ff57026b12b39af45cf5f88afac39b3f
lgh-solace <linda.hillis@solace.com>
2026-03-11 10:30:48 -0400

    chore: toast

--- client/webui/frontend/src/lib/components/toast/Toast.tsx
+++ client/webui/frontend/src/lib/components/toast/Toast.tsx
@@ -6,7 +6,7 @@ import type { Notification } from "../../types";
 export function Toast({ id, message, type }: Notification) {
     return (
         <div id={id} className="transform transition-all duration-200 ease-in-out">
-            <Alert className="light:border-none max-w-80 min-w-58 rounded-sm bg-(--background-wMain) px-4 py-0 text-white shadow-[0_4px_6px_-1px_rgba(0,0,0,0.15)]">
+            <Alert className="light:border-none max-w-80 min-w-58 rounded-sm bg-(--nav-bg) px-4 py-0 text-(--nav-text) shadow-[0_4px_6px_-1px_rgba(0,0,0,0.15)]">
                 <AlertTitle className="flex h-10 items-center">
                     {type === "warning" && <AlertTriangle className="mr-2 text-(--warning-wMain)" />}
                     {type === "success" && <CheckCircle className="mr-2 text-(--success-wMain)" />}
