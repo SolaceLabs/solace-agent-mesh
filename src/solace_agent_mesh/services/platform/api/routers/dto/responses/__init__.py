8f2a1a954f8282f2ce144a18cb8507cc83fa5047
Paul Jones <paul.jones@solace.com>
2026-03-25 06:30:35 -0400

    feat(DATAGO-126640): BE | Test model connection API (#1251)
    * commit
    
    * cleanup
    
    * feature flagged
    
    * adding tests
    
    * more tests
    
    * more cleanup
    
    * removed support for features.yaml, will just go with an env variable for now.
    
    * clean up and unit tests
    
    * fixed unit tests
    
    * remove files that should not be in PR
    
    * clean up
    
    * restore database_helpers.py
    
    * initial commit
    
    * fixed missing import
    
    * fixed up response handling
    
    * cleaned get responses
    
    * update styling
    
    * initial commit
    
    * commit
    
    * fixed description for model seeding.
    
    * update tests
    
    * updated from review comments
    
    * update based on review comments
    
    * fix merge
    
    * cleanup api and tests
    
    * autofill api base
    
    * allow empty api base on post create
    
    * fixed apis to return data responses
    
    * fix migration tests
    
    * feature flag
    
    * unit tests
    
    * updates for code review
    
    * cleanup for stories
    
    * fixed merge error
    
    * cleanup of OnboardingView
    
    * page common stories
    
    * fix lint error
    
    * cleanup
    
    * fixed lint error
    
    * fixed issue with colours being too light and cleanup
    
    * tests
    
    * cleanup
    
    * more cleanup
    
    * remove a todo
    
    * remove todo
    
    * cleanup for code review
    
    * cleanup
    
    * cleanup
    
    * trigger CI
    
    * update based on comments
    
    * commit
    
    * commit
    
    * fixed function name
    
    * use create_data_response
    
    * tests
    
    * Trigger CI
    
    * fixes for unit tests
    
    * fixed unit tests
    
    * fixed another syntax error
    
    * tests for coverage

--- src/solace_agent_mesh/services/platform/api/routers/dto/responses/__init__.py
+++ src/solace_agent_mesh/services/platform/api/routers/dto/responses/__init__.py
@@ -1,7 +1,11 @@
 """Response DTOs for platform service."""
 
-from .model_configuration_responses import ModelConfigurationResponse
+from .model_configuration_responses import (
+    ModelConfigurationResponse,
+    ModelConfigurationTestResponse,
+)
 
 __all__ = [
     "ModelConfigurationResponse",
+    "ModelConfigurationTestResponse",
 ]
