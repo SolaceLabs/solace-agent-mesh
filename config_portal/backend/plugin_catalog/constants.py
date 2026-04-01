e1e4e1a64f9e0e3bc7724b5849956fa239ac2cce
Cyrus Mobini <68962752+cyrus2281@users.noreply.github.com>
2026-04-01 15:20:18 -0400

    fix: simplify installation of sam-rest-gateway plugin by using PyPI instead of Git URL

--- config_portal/backend/plugin_catalog/constants.py
+++ config_portal/backend/plugin_catalog/constants.py
@@ -20,5 +20,19 @@ DEFAULT_OFFICIAL_REGISTRY_URL = (
 OFFICIAL_REGISTRY_GIT_BRANCH = "main"
 IGNORE_OFFICIAL_FLAG_REPOS = []
 
+PUBLISHED_OFFICIAL_PLUGINS_TO_PYPI = [
+    "sam_bedrock_agent",
+    "sam_event_mesh_gateway",
+    "sam_event_mesh_tool",
+    "sam_mcp_server_gateway_adapter",
+    "sam_mongodb",
+    "sam_nuclia_tool",
+    "sam_rag",
+    "sam_rest_gateway",
+    "sam_ruleset_lookup_tool",
+    "sam_slack_gateway_adapter",
+    "sam_sql_database_tool",
+]
+
 USER_REGISTRIES_PATH = SAM_HOME / "plugin_catalog_registries.json"
 PLUGIN_CATALOG_TEMP_DIR = SAM_HOME / "plugin_catalog_tmp"
